296259bea345715c124c7aa1b1e04ec800518eeb
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-11-11 14:34:34 -0300

    Feat: Implementação de fetch de dados CSV
    - Criação do arquivo dataFetcher.js para processamento de dados externos
    - Implementação de função handleProcessError para tratamento de erros e logs
    - Implementação de fetchAndProcessData para buscar dados CSV
    - Adição de sistema de atualização automática a cada 1 minuto
    - Conversão de dados CSV para formato compatível com a API
    - Tratamento de tipos de dados e formatação de data/hora

---
+++ src/dataFetcher.js
@@ -0,0 +1,73 @@
+const url = 'https://detectorderaios.com.br/Dados/EURO.csv'
+
+export async function handleProcessError() {
+  try {
+    console.log('------ Iniciando ciclo de atualização ------')
+    console.log('Hora início:', new Date().toLocaleTimeString())
+
+    await fetchAndProcessData()
+    console.log('Dados processados com sucesso!')
+
+    // Configura próxima execução
+    console.log('------ Configurando próxima execução ------')
+    console.log('Próxima atualização em 1 minuto')
+    setInterval(async () => {
+      console.log('\n------ Novo ciclo iniciando ------')
+      console.log('Hora início:', new Date().toLocaleTimeString())
+      await fetchAndProcessData()
+      console.log('Dados processados com sucesso!')
+      console.log('Próxima atualização em 1 minuto')
+    }, 60000)
+  } catch (error) {
+    console.error('❌ Erro durante processamento:', error.message)
+    console.error('Tipo do erro:', error.name)
+  }
+}
+
+export async function fetchAndProcessData() {
+  try {
+    const response = await fetch(url)
+    const responseText = await response.text()
+
+    const lines = responseText.split('\r\n')
+
+    const headers = lines[0].split(';')
+    const values = lines[1].split(';')
+    const newHeadersArray = headers.slice(1)
+    const newValuesArray = values.slice(1)
+
+    const dateTime = response.headers.get('date')
+    const originalDate = new Date(dateTime)
+    originalDate.setUTCHours(originalDate.getUTCHours() - 3)
+    const dataHora = originalDate.toISOString()
+    const newArray = createObjectFromArrays(
+      newHeadersArray,
+      newValuesArray,
+      dataHora
+    )
+
+    //console.log(headers)
+    console.log(newArray)
+    //console.log(newArray)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function createObjectFromArrays(newHeadersArray, newValuesArray, dataHora) {
+  if (newHeadersArray.length !== newValuesArray.length) {
+    return null
+  }
+
+  const object = newHeadersArray.reduce(
+    (acc, cur, idx) => {
+      return {
+        // biome-ignore lint/performance/noAccumulatingSpread: <explanation>
+        ...acc,
+        [cur]: Number(newValuesArray[idx].replace(',', '.')),
+      }
+    },
+    { dataHora }
+  )
+  return object
+}
